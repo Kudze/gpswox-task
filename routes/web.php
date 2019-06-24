6c2fa663f1e8b7b24e99a6440e0be4361c53a54a
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-06-24 20:39:20 +0300

    Removed welcome page.

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->get('/', function () {
+    return response()->redirectToRoute('home');
 });
 
 Auth::routes();
