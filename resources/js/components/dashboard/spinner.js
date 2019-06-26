9bb1f8035c80aeaf39cf7218aec70444cf777ba2
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-06-26 16:38:43 +0300

    Added small spinner

--- resources/js/components/dashboard/spinner.js
+++ resources/js/components/dashboard/spinner.js
@@ -2,10 +2,8 @@ import React from "react";
 
 export default () => {
     return (
-        <div className="w-100 vh-75 d-flex justify-content-center align-items-center">
-            <div className="spinner-border spinner-border-xl" role="status">
-                <span className="sr-only">Loading...</span>
-            </div>
+        <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
         </div>
     )
 }
