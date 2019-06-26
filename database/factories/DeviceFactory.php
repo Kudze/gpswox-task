7ee7c397e3ec9fe9f00f82dcfbf08c8abed3a575
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-06-26 03:43:57 +0300

    Added device and user table seeding.

--- database/factories/DeviceFactory.php
+++ database/factories/DeviceFactory.php
@@ -7,6 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Device::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'imei' => $faker->numberBetween(
+            pow(10, 14),
+            pow(10, 15) - 1
+        ),
+        'latitude' => $faker->latitude,
+        'longitude' => $faker->longitude
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Database\Seeder;
 
+const DEVICE_COUNT = 100000;
+const USER_COUNT = 50;
+const DEVICE_PER_USER_COUNT = 5000;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(App\User::class, USER_COUNT)->create();
+        factory(App\Device::class, DEVICE_COUNT)->create();
+
+        $devices = App\Device::all();
+
+        App\User::all()->each(
+            function ($user) use ($devices) {
+            $user->devices()->attach(
+                $devices->random(DEVICE_PER_USER_COUNT)->pluck('id')->toArray()
+            );
+        });
     }
 }
