d7430cf5c5610f3e29dccaaf76da82d3e0fdbe48
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-06-26 04:17:34 +0300

    Custom spinner component created

--- resources/js/components/dashboard/spinner.js
+++ resources/js/components/dashboard/spinner.js
@@ -1,7 +1,9 @@
+import React from "react";
+
 export default () => {
     return (
-        <div className="w-100 vh-75 d-flex justify-content-center">
-            <div className="spinner-border" role="status">
+        <div className="w-100 vh-75 d-flex justify-content-center align-items-center">
+            <div className="spinner-border spinner-border-xl" role="status">
                 <span className="sr-only">Loading...</span>
             </div>
         </div>
