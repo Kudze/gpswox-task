d08de8fffd8486c689327ebf3859b2d93be91c44
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-06-26 04:17:10 +0300

    Added more breakpoints for bootstrap vh support. And added xl spinner

---
+++ resources/js/components/dashboard/spinner.js
@@ -0,0 +1,9 @@
+export default () => {
+    return (
+        <div className="w-100 vh-75 d-flex justify-content-center">
+            <div className="spinner-border" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+        </div>
+    )
+}
