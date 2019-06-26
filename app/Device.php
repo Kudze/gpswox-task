d8064180e19c3596814291cd82489ea802debc00
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-06-26 03:09:55 +0300

    It appears I need to specify pivot collumns in relationship declaration too

--- app/Device.php
+++ app/Device.php
@@ -8,6 +8,6 @@ class Device extends Model
 {
     public function users()
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class)->withPivot('active');
     }
 }
