d8c0f4e79dbad6bd5cfb75970dc7612958d94679
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-06-27 18:55:07 +0300

    If no devices are available sidemenu will show a message click on x to add more devices

--- resources/js/components/dashboard/sideMenu.js
+++ resources/js/components/dashboard/sideMenu.js
@@ -3,6 +3,28 @@ import React from 'react';
 import DeviceForm from "./deviceForm";
 
 export default (props) => {
+    let renderEmpty = () => {
+        return (
+            <div
+                className={"w-100 d-flex align-items-center justify-content-center text-center"}
+                style={{height: "calc(75vh - 80px)"}}
+            >
+                <h3>Click "Create Device" in order to add some devices!</h3>
+            </div>
+        )
+    };
+
+    let renderList = () => {
+        return (
+            <div
+                className={"w-100 overflow-auto"}
+                style={{height: "calc(75vh - 80px)"}}
+            >
+                {props.children}
+            </div>
+        )
+    };
+
     return (
         <div
             className={"w-100 vh-75 d-flex flex-column justify-content-between align-items-center"}
@@ -12,9 +34,7 @@ export default (props) => {
                 }
             }
         >
-            <div className={"w-100 overflow-auto justify-conent-center"} style={{height: "calc(75vh - 80px)"}}>
-                {props.children}
-            </div>
+            {props.children.length === 0 ? renderEmpty() : renderList()}
             <DeviceForm/>
         </div>
     )
