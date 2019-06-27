868841b20c511f0fc892ae7ffa065372ec493ca6
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-06-27 15:51:37 +0300

    Removed some leftover unused code

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -6,21 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Device extends Model
 {
+
     public function users()
     {
         return $this->belongsToMany(User::class)->withPivot('active');
     }
 
-    /**
-     * I wasnt really sure what is the best algorithm for this.
-     * So just though up a new one.
-     * But its fast enough.
-     *
-     * **Also for a big project this would go into repository but its not necceseary for this small scale project.
-     */
-    public function closestDevice() {
-
-        //For context we want to get how many devices there is in the database.
-
-    }
 }
