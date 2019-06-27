703e087b513beba638615f60d40580635a70f073
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-06-28 00:57:22 +0300

    SQL query fixed for calculating distance of lat & lng. We live on a sphere not a square...

--- app/Repositories/DeviceRepository.php
+++ app/Repositories/DeviceRepository.php
@@ -19,9 +19,16 @@ class DeviceRepository implements DeviceRepositoryInterface
             ->selectRaw(
             //Since we only want to find closest point and we don't want to know exact distance in kilometres
             //We can skip sqrt and pow parts in the calculation, and it still should be fine.
-                "imei, ABS(latitude - :lat) + ABS(longitude - :lng) AS distance",
+                "imei, 
+                            111.111 * DEGREES(ACOS(LEAST(COS(RADIANS(latitude))
+                             * COS(RADIANS(:lat))
+                             * COS(RADIANS(longitude - :lng))
+                             + SIN(RADIANS(latitude))
+                             * SIN(RADIANS(:latt)), 1.0))) 
+                             AS distance",
                 [
                     'lat' => $latitude,
+                    'latt' => $latitude, //It didnt allow for me to use same arg twice.
                     'lng' => $logitude
                 ]
             )
