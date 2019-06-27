8bc2662708874987e9cd60c86ce957f6cfe76ecb
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-06-28 00:43:09 +0300

    Added role:set-admin {email} to easily make user admin

---
+++ app/Console/Commands/GiveAdminRole.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Psr\Log\InvalidArgumentException;
+use Spatie\Permission\Models\Role;
+
+class GiveAdminRole extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'role:set-admin {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Gives user an admin role';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $email = $this->argument("email");
+
+        $user = User::where(["email" => $email])->first();
+
+        if ($user === null)
+            $this->error("Email is invalid!");
+
+        $webAdmin = Role::firstOrCreate(['guard_name' => 'web', 'name' => 'admin']);
+        $apiAdmin = Role::firstOrCreate(['guard_name' => 'api', 'name' => 'admin']);
+        $user->assignRole($webAdmin);
+        $user->assignRole($apiAdmin);
+
+        $this->info("Success!");
+    }
+}
