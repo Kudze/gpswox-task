4cfcef61113dc8b4f8cba99ee4cdb89cdb0bada1
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-06-27 17:52:45 +0300

    Spacing improvements

--- resources/js/components/dashboard/deviceItem.js
+++ resources/js/components/dashboard/deviceItem.js
@@ -17,7 +17,7 @@ class DeviceItem extends React.Component {
 
     render() {
         return (
-            <div className={"d-block"} style={this.props.marked ? {backgroundColor: "rgba(0,0,0,0.5)"} : {}}>
+            <div className={"d-block px-2 py-2"} style={this.props.marked ? {backgroundColor: "rgba(0,0,0,0.5)"} : {}}>
                 <div className="d-flex w-100 align-items-center justify-content-start">
                     <div style={{height: "50px", width: "50px"}}>
                         <input className="d-block" type="checkbox"
@@ -28,7 +28,7 @@ class DeviceItem extends React.Component {
                     </div>
                     <div className={"ml-4"}>
                         <h3>{this.props.data.name} {this.props.marked ? "(Marked)" : ""}</h3>
-                        <p>{this.props.data.imei}</p>
+                        <p className={"mb-0"}>{this.props.data.imei}</p>
                     </div>
                 </div>
             </div>
